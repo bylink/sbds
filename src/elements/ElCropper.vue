1032a1a19f2faec4eaa2fe0c7be7bfbe2932e7aa
Shvg <itdzuj@gmail.com>
2019-09-23 13:57:30 +0300

    fixes

--- src/elements/ElCropper.vue
+++ src/elements/ElCropper.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="el-cropper">
-    <VueCropper
-      ref="cropper"
-      :aspect-ratio="1 / 1"
-      :initialAspectRatio="1 / 1"
-      :highlight="false"
-      :guides="false"
-      :view-mode="2"
-      drag-mode="crop"
-      :background="true"
-      :src="imgSrc"
-      alt="Avatar"
-    >
-    </VueCropper>
+    <VueCropper ref="cropper" :src="imgSrc" alt="Avatar"> </VueCropper>
   </div>
 </template>
 
@@ -24,84 +12,13 @@ export default {
   components: {
     VueCropper,
   },
-  data() {
-    return {
-      cropImg: "",
-    }
-  },
   props: {
     imgSrc: {},
-  },
-  watch: {
-    imgSrc(val) {
-      this.setImage(val)
-    },
-  },
-  methods: {
-    setImage() {
-      this.$refs.cropper.replace(this.imgSrc)
-    },
-    cropImage() {
-      this.$refs.cropper
-        .getCroppedCanvas({
-          maxWidth: 500,
-          maxHeight: 500,
-        })
-        .toBlob(blob => {
-          this.$emit("crop", blob)
-        })
-    },
   },
 }
 </script>
 
-<style lang="scss">
-.el-cropper {
-  $block-name: &;
-
-  $color-main-cropp: $color_mint;
-
-  .cropper {
-    &-modal {
-      opacity: 0.7;
-      background-color: $color-white;
-    }
-
-    &-face,
-    &-line,
-    &-point {
-      opacity: 0;
-    }
-
-    &-view-box {
-      border-radius: $radius-circle;
-      outline: 1px dashed $color-main-cropp;
-    }
-
-    &-line {
-      background-color: $color-main-cropp;
-    }
-
-    &-point {
-      background: $color-main-cropp;
-
-      &.point-se {
-        @media #{$media-query-xl} {
-          opacity: 1;
-        }
-
-        &:before {
-          background-color: $color-main-cropp;
-        }
-      }
-    }
-
-    &-center {
-      display: none;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
 
 <docs>
     ```jsx
